57684d8fafcc21506cec0f35c40861a0f0387be5
Sander Voss <sander@rolisa.nl>
2017-06-10 11:22:05 +0200

    Update to the material reader. It is no longer necessary to read all the nr of plies by themselfs but can now be accessed in one go.

--- bin/csv_read.py
+++ bin/csv_read.py
@@ -4,8 +4,11 @@ import csv
 from parapy.core import *
 
 class ReadMaterial(Base):
-    ply_file = Input() 
-    n_layers = Input()
+    """
+    This class generates an object with a read attribute that will output a the dictionary of a certain material csv file
+    It contains all the number of plies and can be accessed through <dict_name>["<nr_plies>"]["<Property>"]
+    """
+    ply_file = Input()
     materials = {}
 
     @Attribute
@@ -20,12 +23,12 @@ class ReadMaterial(Base):
         with open(path, 'rb') as file:
             reader = csv.reader(file, delimiter=',', quotechar='|')
             row_names = next(reader)    # gets the first line
-            n_layers = self.n_layers    # Sets the number of layers
             for row in reader:
-                if row[0] == str(n_layers):
-                    print row
-                    for x in range(0, len(row)):
-                        var_name = row_names[x]
-                        value = float(row[x])
-                        self.materials[var_name] = value
+                sub_dict = {}
+                n_layers = row[0]
+                for x in range(0, len(row)):
+                    var_name = row_names[x]
+                    value = float(row[x])
+                    sub_dict[var_name] = value
+                self.materials[n_layers] = sub_dict
         return self.materials
